8af64bfcea0adcb763d9dd44a193ba1fc05240f0
codeinsack <codeinsack@gmail.com>
2022-03-08 21:14:01 +0300

    display form

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,23 @@
 import ReactDOM from "react-dom";
+import { useState } from "react";
 
 const App = () => {
-  return <h1>Hi there!</h1>;
+  const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
+
+  const onClick = () => {
+    console.log(input);
+  };
+
+  return (
+    <div>
+      <textarea value={input} onChange={(e) => setInput(e.target.value)} />
+      <div>
+        <button onClick={onClick}>Submit</button>
+        <pre>{code}</pre>
+      </div>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
