f01383850606197e87fd564c9584d15dcdd42333
codeinsack <codeinsack@gmail.com>
2022-03-13 12:26:57 +0300

    finish with text editor

--- src/components/TextEditor/TextEditor.tsx
+++ src/components/TextEditor/TextEditor.tsx
@@ -5,6 +5,7 @@ import { FC, useEffect, useState, useRef } from "react";
 const TextEditor: FC = () => {
   const ref = useRef<HTMLDivElement | null>(null);
   const [editing, setEditing] = useState(false);
+  const [value, setValue] = useState("# Header");
 
   useEffect(() => {
     const listener = (event: MouseEvent) => {
@@ -23,15 +24,17 @@ const TextEditor: FC = () => {
 
   if (editing) {
     return (
-      <div ref={ref}>
-        <MDEditor />
+      <div className="text-editor" ref={ref}>
+        <MDEditor value={value} onChange={(v) => setValue(v || "")} />
       </div>
     );
   }
 
   return (
-    <div onClick={() => setEditing(true)}>
-      <MDEditor.Markdown source={"# Header"} />
+    <div className="text-editor card" onClick={() => setEditing(true)}>
+      <div className="card-content">
+        <MDEditor.Markdown source={value} />
+      </div>
     </div>
   );
 };
